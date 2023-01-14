80de00949d4747cd4145d8077c4b96449258a46d
empsps <sophialealbarros@gmail.com>
2023-01-14 19:57:30 -0300

    style(auth): add some new lines

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,12 +5,15 @@ import { RegisterDTO } from './AuthModel';
 export class AuthController {
   async register(req: Request, res: Response) {
     const { email, password, username } = req.body;
+
     const newUser = new RegisterDTO();
     Object.assign(newUser, { email, password, username });
+
     validate(newUser, { validationError: { target: false, value: false } }).then((errors) => {
       if (errors.length) {
         return res.status(400).json(errors);
       }
+
       return res.json(newUser);
     });
   }
