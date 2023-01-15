56ed8231637e6a57778701404990cbdc2733cbb7
empsps <sophialealbarros@gmail.com>
2023-01-15 19:52:07 -0300

    setup sessions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import session from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { routes } from './routes';
+import cookieParser from 'cookie-parser';
+import prisma from './prisma';
 
 dotenv.config();
 
+if (!process.env.SESSION_SECRET) {
+  throw new Error('Error: a SESSION_SECRET must be set.');
+}
+
 const server = express();
 server.use(express.json());
+server.use(
+  session({
+    cookie: {
+      maxAge: 31560000000000,
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdFunction: undefined,
+      dbRecordIdIsSessionId: true,
+    }),
+  }),
+);
 
 // Import all routes
 server.use('/', routes);
